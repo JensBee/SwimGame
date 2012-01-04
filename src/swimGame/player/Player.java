3aba8a6ffe656ecc0f7d1f7c8fdd76ef20830e1e
Jens Bertram <code@jens-bertram.net>
2012-01-04 13:27:58 +0100

    squeezed rating calculation to a way simpler approach

--- src/swimGame/player/Player.java
+++ src/swimGame/player/Player.java
@@ -18,7 +18,7 @@ public class Player extends AbstractPlayer {
 	 */
 	private final byte selfRiskyness = 9;
 
-	private final InnerCardRating cardRating = new InnerCardRating();
+	private final Cards cards = new Cards();
 
 	/** Constructor */
 	public Player() {
@@ -33,15 +33,27 @@ public class Player extends AbstractPlayer {
 	 * @author Jens Bertram <code@jens-bertram.net>
 	 * 
 	 */
-	private class InnerCardRating {
-		public int rateStack(CardStack cardStack) {
-			return this.rateStackByArray(cardStack.getArray());
+	private class Cards {
+		CardStack cs;
+
+		/**
+		 * Constructor
+		 */
+		protected Cards() {
 		}
 
-		private int rateStackByArray(byte[][] cardStack) {
-			this.rateMaxStackValue(cardStack);
-			return 0;
+		protected void setCardStack(CardStack cs) {
+			this.cs = cs;
 		}
+
+		// public int rateStack(CardStack cardStack) {
+		// return this.rateStackByArray(cardStack.getArray());
+		// }
+		//
+		// private int rateStackByArray(byte[][] cardStack) {
+		// this.rateMaxStackValue(cardStack);
+		// return 0;
+		// }
 
 		/**
 		 * Calculates the value of a card belonging to a stack
@@ -61,23 +73,56 @@ public class Player extends AbstractPlayer {
 			}
 		}
 
-		private int rateMaxStackValue(byte[][] cardStack) {
-			// discount by 9 for each card (9 is avg card value)
-			// missing a color-card counts two times the discount
-			int foundCards[] = new int[] { -1, -1, -1 };
-			int cardIndex = 0;
+		// private int rateMaxStackValue(byte[][] cardStack) {
+		// // discount by 9 for each card (9 is avg card value)
+		// // missing a color-card counts two times the discount
+		// int foundCards[] = new int[] { -1, -1, -1 };
+		// int cardIndex = 0;
+		//
+		// // iterate over every color
+		// for (int color = 0; color < (CardStack.CARDS_MAX_COLOR); color++) {
+		// // iterate over every card
+		// for (int card = 0; card < (CardStack.CARDS_MAX_CARD); card++) {
+		// if (cardStack[card][color] == 1) {
+		// foundCards[cardIndex++] = card;
+		// }
+		// }
+		// }
+		// return 0;
+		// }
 
-			// iterate over every color
-			for (int color = 0; color < (CardStack.CARDS_MAX_COLOR); color++) {
-				// iterate over every card
-				for (int card = 0; card < (CardStack.CARDS_MAX_CARD); card++) {
-					if (cardStack[card][color] == 1) {
-						foundCards[cardIndex++] = card;
-						// int value = this.getStackCardValue(card);
-					}
+		/**
+		 * 
+		 * @param card
+		 *            int[card][color]
+		 * @return
+		 */
+		protected int rateCard(int[] aCard) {
+			int cardValue = 0;
+			byte same[] = new byte[] { 0, 0 }; // [card, color]
+
+			// go through cards by same color
+			for (int card = 0; card < CardStack.CARDS_MAX_CARD; card++) {
+				// ..to check if we own it..
+				if (this.cs.getArray()[card][aCard[1]] == 0) {
+					// ..no we don't - check next
+					continue;
 				}
+				same[1] = same[1]++;
+				cardValue = cardValue + this.getStackCardValue(card);
 			}
-			return 0;
+
+			// go through cards by same type
+			for (int color = 0; color < (CardStack.CARDS_MAX_COLOR); color++) {
+				// ..to check if we own it..
+				if (this.cs.getArray()[aCard[0]][color] == 0) {
+					// ..no we don't - check next
+					continue;
+				}
+				same[0] = same[0]++;
+				cardValue = cardValue + this.getStackCardValue(aCard[0]);
+			}
+			return cardValue;
 		}
 	}
 
@@ -90,9 +135,10 @@ public class Player extends AbstractPlayer {
 		byte[] cards = this.rateCards();
 		Debug.println(
 				this,
-				"Looks I'll drop "
-						+ this.cardUtils.cardToString(new int[] { cards[6],
-								cards[7] }) + ".");
+				String.format(
+						"Looks I'll drop %s.",
+						this.cardUtils.cardToString(new int[] { cards[6],
+								cards[7] })));
 		return true;
 	}
 
@@ -112,125 +158,36 @@ public class Player extends AbstractPlayer {
 	 */
 	private byte[] rateCards() {
 		// get a copy of out card-stack
-		byte[][] cardStack = this.cardStack.getArray();
+		byte[][] cardStackArray = this.cardStack.getArray();
 		// store three cards (x,y) with one rating (z) each
 		final byte[] rating = new byte[9];
 		// rating array index pointer
 		int ratingCount = 0;
 
 		Debug.println(this, "Rating my current cards..");
+		this.cards.setCardStack(this.cardStack);
 
 		// dig through the card-stack by color-rows..
 		for (int color = 0; color < (CardStack.CARDS_MAX_COLOR); color++) {
-			// track, how many cards of one color we own
-			int sameColor = 0;
-			// store the last card we've seen
-			byte[] lastCard = new byte[] { 0, 0 };
-
-			// ..and iterate over every card..
-			for (int card = 0; card < (CardStack.CARDS_MAX_CARD); card++) {
-				// the value witch represents the rating for this card
-				int cardValue = 0;
-				// is our card already rated?
-				boolean rated = false;
-
-				// ..to check if we own it..
-				if (cardStack[card][color] == 0) {
-					// ..no we don't
+			// ..and iterate over every card
+			for (int card = 0; card < CardStack.CARDS_MAX_CARD; card++) {
+				if (cardStackArray[card][color] == 0) {
+					// ..no we don't - check next
 					continue;
 				}
-
-				// We found one, so check surroundings. Going back makes no
-				// sense. Going forward is already handled by the wrapping
-				// loop, so check downwards (if possible).
-				int sameType = 0;
-				sameColor++;
-
-				cardValue = this.cardRating.getStackCardValue(card);
-
-				// see if we already own a card of the current color
-				if (sameColor > 1) {
-					// we do, so rate up the previous and the current card
-					if (sameColor == 2) {
-						// upgrade last seen card of current color
-						ratingCount = ratingCount - 3;
-						rating[ratingCount++] = lastCard[0];
-						rating[ratingCount++] = (byte) color;
-						rating[ratingCount++] = (byte) (lastCard[1] + (11 - this.selfRiskyness));
-
-						Debug.println(
-								this,
-								"Re-rating "
-										+ this.cardUtils
-												.cardToString(new int[] {
-														lastCard[0], color })
-										+ " (color): "
-										+ rating[ratingCount - 1]);
-					}
-					cardValue = (byte) (cardValue + (15 - this.selfRiskyness));
-				}
-
-				for (int colDig = color + 1; colDig < (CardStack.CARDS_MAX_COLOR); colDig++) {
-					if (this.cardStack.getArray()[card][colDig] == 1) {
-						// current row
-						rating[ratingCount++] = (byte) card;
-						rating[ratingCount++] = (byte) color;
-						rating[ratingCount++] = (byte) (cardValue + (11 - this.selfRiskyness));
-						Debug.println(
-								this,
-								"Rating "
-										+ this.cardUtils
-												.cardToString(new int[] { card,
-														color })
-										+ " (similarity): "
-										+ rating[ratingCount - 1]);
-
-						// one row down
-						rating[ratingCount++] = (byte) card;
-						rating[ratingCount++] = (byte) colDig;
-						rating[ratingCount++] = (byte) (cardValue + (10 - this.selfRiskyness));
-						Debug.println(
-								this,
-								"Rating "
-										+ this.cardUtils
-												.cardToString(new int[] { card,
-														colDig })
-										+ " (similarity): "
-										+ rating[ratingCount - 1]);
-						// remove this card from out temporary stack so we don't
-						// inspect it twice
-						cardStack[card][colDig] = 0;
-						sameType++;
-						rated = true;
-					}
-				}
-
-				// do some final testing for special / general case
-				if (sameType == 2) {
-					// 30.5 points all of a type
-					Debug.println(this, "Yay, JACKPOT! (all cards same type)");
-				} else if ((sameType == 0) && (rated == false)) {
-					// nothing special, just a single unrated card
-					// rating[card][color] = cardValue;
-					rating[ratingCount++] = (byte) card;
-					rating[ratingCount++] = (byte) color;
-					rating[ratingCount++] = (byte) cardValue;
-					Debug.println(
-							this,
-							"Rating "
-									+ this.cardUtils.cardToString(new int[] {
-											card, color }) + ": "
-									+ rating[ratingCount - 1]);
-				}
-
-				if (sameColor > 2) {
-					Debug.println(this, "Yay, COLORPOT! (all cards same color)");
-				}
-
-				lastCard = new byte[] { (byte) card, (byte) cardValue };
+				// we do - so rate it
+				int cardRating = this.cards.rateCard(new int[] { card, color });
+				Debug.println(
+						this,
+						String.format(
+								"* NEW Rating %s: %d",
+								this.cardUtils.cardToString(new int[] { card,
+										color }), cardRating));
+				rating[ratingCount++] = (byte) card;
+				rating[ratingCount++] = (byte) color;
+				rating[ratingCount++] = (byte) cardRating;
 			}
 		}
-
 		return this.rankCardRating(rating);
 	}
 
